c6b2a98e53a8276edd36afbfcbcd96ff907a5690
Lee Hoyong <igotoweb@gmail.com>
2018-06-16 02:37:19 +0900

    add introduction and modify index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,49 @@
-function addClickEvent(element, handler) {
-  element.addEventListener('click', handler);
+var menusToCalculate = [];
+
+function generateMenuButton(menu) {
+  return $('<button>').text(menu.name)
+                      .addClass('menu')
+                      .data('price', menu.price)
+                      .on('click', menu, getMenuDataFromButton);
+  /*
+   * 이 내용을 알고 샆다면 여기를 참조하세요. 
+   * $('<button>') : https://api.jquery.com/jQuery/#jQuery2
+   * .data method : https://api.jquery.com/data/
+   * .on method : https://api.jquery.com/on/
+   */
 }
 
+function getMenuDataFromButton(event) {
+  console.log(event.data);
+
+  // 계산할 내용을 menusToCalculate에 추가해보면 어떨까요?
+}
+
+function renderMenu(renderTo) {
+  MENU_DATA.forEach(function(menu) {
+    generateMenuButton(menu).appendTo(renderTo);
+  });
+}
+
+// 아래 함수들은 힌트로 주어진 함수들입니다. console에서 실행해 보세요
 function addRow() {
-  var tr = document.createElement('tr');
-  for(var i=0; i<arguments.length; i++) {
-    var td = document.createElement('td');
-    var content = document.createTextNode(arguments[i]);
-    td.appendChild(content);
-    tr.appendChild(td);
-  }
-  document.querySelector('table').appendChild(tr)
+  var elementQueue = $('#queue');
+  generateRow().appendTo(elementQueue);
 }
 
-function getButtonInfo(e, data) {
-  console.log(e.target.dataset);
-  console.log(e.target.innerHTML)
+function generateRow() {
+  var elementTr = $('<tr>');
+  $('<td>').text('메뉴1').appendTo(elementTr);
+  $('<td>').text('1').appendTo(elementTr);
+  $('<td>').text('1000').appendTo(elementTr);
+  return elementTr;
+}
+// 힌트 끝
+
+
+function initialize() {
+  var elementMenu = $('#menu-container');
+  renderMenu(elementMenu);
 }
 
-/* hint */
-
-// var buttons = document.querySelectorAll('button');
-// addClickEvent(document.querySelector('#thisIsID'), getButtonInfo);
+initialize();
---
+++ js/menu.js
@@ -0,0 +1,10 @@
+var MENU_DATA = [
+  {
+    name: '아메리카노',
+    price: 2000
+  },
+  {
+    name: '까페라떼',
+    price: 2500
+  }
+];
