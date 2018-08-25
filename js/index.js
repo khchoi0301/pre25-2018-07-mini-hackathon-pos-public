64e28bd446523e2f2698c1ce4affe636660c7e38
kihong choi <khchoi0301@gmail.com>
2018-08-25 22:00:00 +0900

    Make POS

--- js/index.js
+++ js/index.js
@@ -12,10 +12,43 @@ function generateMenuButton(menu) {
    * .on method : https://api.jquery.com/on/
    */
 }
+var count=[0,0,0,0]
+var cost=[2000,2500,0,0]
+
+function minus(event){
+  $('.minus').off('click').on('click',function(){
+    count[1]--
+    $('.까페라떼').remove();
+    $('.합계').remove();
+    console.log(count[1],cost[0])
+    addRow('까페라떼',count[1],cost[1]);
+
+    console.log('hi');
+  })
+}
 
 function getMenuDataFromButton(event) {
-  console.log(event.data);
+  console.log(event.data)
+    
+  if(event.data.name==="아메리카노"){
+    $('.아메리카노').remove();
+    $('.합계').remove();
+    count[0]++
+    cost[0]=count*event.data.price
+    addRow(event.data.name,count[0],event.data.price)
+    minus();
+  } 
 
+  if(event.data.name==="까페라떼"){
+    $('.까페라떼').remove();
+    $('.합계').remove();
+
+    count[1]++
+    cost[1]=count*event.data.price
+    addRow(event.data.name,count[1],event.data.price)
+    minus();
+  } 
+ 
   // 계산할 내용을 menusToCalculate에 추가해보면 어떨까요?
 }
 
@@ -26,24 +59,48 @@ function renderMenu(renderTo) {
 }
 
 // 아래 함수들은 힌트로 주어진 함수들입니다. console에서 실행해 보세요
-function addRow() {
+function addRow(a,b,c) {
   var elementQueue = $('#queue');
-  generateRow().appendTo(elementQueue);
+  generateRow(a,b,c).appendTo(elementQueue);
+  generateRow2(a,b,c).appendTo(elementQueue);
 }
 
-function generateRow() {
-  var elementTr = $('<tr>');
-  $('<td>').text('메뉴1').appendTo(elementTr);
-  $('<td>').text('1').appendTo(elementTr);
-  $('<td>').text('1000').appendTo(elementTr);
+function generateRow(a,b,c) {
+  var elementTr = $('<tr class='+a+'>');
+
+ 
+  $('<td>').text(a).appendTo(elementTr);
+  $('<td>').text(b).appendTo(elementTr);
+  $('<td><button class=minus>-</button></td>').appendTo(elementTr);
+  $('<td>').text(b*c).appendTo(elementTr);
   return elementTr;
+}
+function generateRow2(a,b,c) {
+  var elementTr2 = $('<tr class=합계>');
+  /*
+  $('<td>').text(a).prependTo(elementTr);
+  $('<td>').text(b).prependTo(elementTr);
+  $('<td>').text(b*2500).prependTo(elementTr);
+  */
+ 
+  $('<td>').text('합계').appendTo(elementTr2);
+  $('<td>').text(count[0]+count[1]).appendTo(elementTr2);
+  $('<td>').text('').appendTo(elementTr2);
+  $('<td>').text(count[0]*2000+count[1]*2500).appendTo(elementTr2);
+  return elementTr2;
 }
 // 힌트 끝
 
 
 function initialize() {
   var elementMenu = $('#menu-container');
+  
   renderMenu(elementMenu);
 }
 
 initialize();
+
+
+//td와 버튼의 통합    <td><button></button></td>
+//event와 data.name ??
+//minus 펑션의 반복 실행   on off로 해결??
